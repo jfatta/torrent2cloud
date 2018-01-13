9afb3077a9841b378fb53e5005b88661fc84c17d
Jorge L. Fatta <jorge.fatta@southworks.net>
2018-01-13 10:35:40 -0300

    Upload to dbox

--- spike/ttcSpike.js
+++ spike/ttcSpike.js
@@ -1,5 +1,8 @@
 var WebTorrent = require('webtorrent')
 var fs = require('fs')
+const db = require('dropbox-stream');
+const TOKEN = '';
+//const FILETOUPLOAD = '/CargoJs/';
 
 var client = new WebTorrent()
 var torrentId = 'magnet:?xt=urn:btih:0d158ad5ece9f95cdbc13caa3c1be977b4b74832&dn=Justice.League.2017.1080p.KORSUB-JesusLovesYew%5BEtHD%5D&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
@@ -8,13 +11,28 @@ client.add(torrentId, function (torrent) {
     // Torrents can contain many files. Let's use the .mp4 file
     torrent.files.forEach(function(file){
         // do something with file
-        var wstream = fs.createWriteStream('Download/' + file.name);
+        
         var stream = file.createReadStream()
         stream.on('data', (chunk) => {
             console.log(`Received ${chunk.length} bytes of data.`);
           });
 
-          stream.pipe(wstream);
+          //var wstream = fs.createWriteStream('Download/' + file.name);
+
+         // stream.pipe(wstream);
+
+
+          var dbox = db.createDropboxUploadStream({
+            token: TOKEN,
+            filepath: '/' + torrent.name + '/' + file.name,
+            chunkSize: 1000 * 1024,
+            autorename: true
+          })
+          .on('error', err => console.log(err))
+          .on('progress', res => console.log(res))
+          .on('metadata', metadata => console.log('Metadata', metadata))
+
+          stream.pipe(dbox);
      })
 
      torrent.on('download', function (bytes) {
